8662ebe7b2d938cd4bb578cfe9e6d3e05fbe9565
kobaruto-star <hkobayashi@dl-techx-xedge.net>
2025-12-25 15:51:37 +0900

    20251225v1

--- src/stores/newsStore.js
+++ src/stores/newsStore.js
@@ -2,6 +2,9 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 import { ref } from 'vue'
 
+const functionUrl =
+  'https://m3h-kobayashi-getnews2-chfqaufjfdfghxgx.japaneast-01.azurewebsites.net/api/getnews'
+
 export const useNewsStore = defineStore('news', {
   state: () => ({
     newslist: ref([]),
@@ -9,9 +12,7 @@ export const useNewsStore = defineStore('news', {
   actions: {
     async getnews() {
       console.log('ニュース取得開始')
-      let response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=c4aa24bd2cc144a1a8d425958dcb0528',
-      )
+      let response = await axios.get(functionUrl)
       this.newslist = response.data.articles
       console.log(this.newslist)
     },
